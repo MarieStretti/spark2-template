a6f7d7d176c494a93a3c9ceae2945bf46ca23565
MarieStr <mariestretti@gmail.com>
2020-01-06 12:18:12 +0100

    spark ex2

--- src/main/scala/sql_practice/examples.scala
+++ src/main/scala/sql_practice/examples.scala
@@ -33,6 +33,38 @@ object examples {
       .orderBy($"tourPrice".desc)
       .show(20)
 
+  }
 
+  def exec2(): Unit = {
+    val spark = SessionBuilder.buildSession()
+    import spark.implicits._
+
+    val pop = spark.read
+      .option("multiline", false)
+      .option("mode", "PERMISSIVE")
+      .json("data/input/demographie_par_commune.json")
+    pop.show
+
+    pop
+      .agg(sum($"Population"))
+      .show
+
+    val agg = pop
+      .groupBy($"Departement")
+      .agg(sum($"Population")
+        .as("totalPop"))
+      .orderBy($"totalPop".desc)
+
+    agg
+      .show
+
+    val dep = spark.read
+      .option("multiline", false)
+      .option("mode", "PERMISSIVE")
+      .csv("data/input/departements.txt")
+      .select($"_c0".as("DepartementName"),$"_c1".as("Departement"))
+    dep.show()
+
+    agg.join(dep,Seq("Departement")).show
   }
 }
