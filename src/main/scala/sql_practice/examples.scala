595cae1c49b15f2ae3b405e6d4cc5994f784f94a
MarieStr <mariestretti@gmail.com>
2020-01-06 14:18:58 +0100

    spark ex3

--- src/main/scala/sql_practice/examples.scala
+++ src/main/scala/sql_practice/examples.scala
@@ -59,12 +59,57 @@ object examples {
       .show
 
     val dep = spark.read
-      .option("multiline", false)
       .option("mode", "PERMISSIVE")
       .csv("data/input/departements.txt")
       .select($"_c0".as("DepartementName"),$"_c1".as("Departement"))
     dep.show()
 
     agg.join(dep,Seq("Departement")).show
+  }
+
+  def exec3(): Unit = {
+    val spark = SessionBuilder.buildSession()
+    import spark.implicits._
+
+    val s7 = spark.read
+      .option("mode", "PERMISSIVE")
+      .option("delimiter", "\\t")
+      .csv("data/input/sample_07")
+        .select($"_c0".as("code"),$"_c1".as("description"),$"_c2".as("nbPeople7"),$"_c3".as("salary7"))
+    s7.show
+
+    val s8 = spark.read
+      .option("mode", "PERMISSIVE")
+      .option("delimiter", "\\t")
+      .csv("data/input/sample_08")
+      .select($"_c0".as("code"),$"_c1".as("description"),$"_c2".as("nbPeople8"),$"_c3".as("salary8"))
+    s8.show
+
+    s7
+    .select($"description",$"salary7")
+      .filter($"salary7">100000)
+    .orderBy($"salary7".desc)
+      .limit(10)
+    .show
+
+    s7
+      .join(s8,Seq("code"))
+      .withColumn("growth", $"salary8" - $"salary7")
+    .filter($"salary7" < $"salary8")
+    .orderBy($"growth".desc)
+      .limit(50)
+      .show
+
+    s7
+      .join(s8,Seq("code"))
+      .withColumn("loss", $"nbPeople8" - $"nbPeople7")
+    .filter($"salary7" > 100000)
+    .orderBy($"loss".asc)
+    .limit(50)
+      .drop(s8("description"))
+      .drop("salary7")
+      .drop("salary8")
+      .drop("code")
+      .show
   }
 }
